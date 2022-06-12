1af284ad8c7b3e354a0ceca07afff22240b61f22
Jordan Litsas <jordanlitsas@live.com.au>
2022-06-12 14:42:49 +1000

    show expenses

--- public/js/addCategory.js
+++ public/js/addCategory.js
@@ -1,8 +1,27 @@
 
 window.addEventListener('DOMContentLoaded', (event) => {
     document.querySelector("#addCategoryBtn").onclick = addCategory;
+    getExpenses();
 });
 
+const getExpensese = async() => {
+    document.querySelector('#res').value = "some text";
+
+    await $.ajax({
+        async: true,
+        url: `https://expense-tracker-springboot-api.herokuapp.com/spend/${sessionStorage.getItem("userId")}`,
+        type: 'GET',
+        success: function(data, responseText, jqXHR){
+           document.querySelector('#res').textContent = data;
+        }, 
+        error: function(error){
+            console.log('user not inserted')
+            console.log(error)
+            
+
+        }
+    })
+}
 const addCategory = async () => {
     let name = document.querySelector("#categoryNameInput").value;
     let sevenDayLimit = document.querySelector("#sevenDayLimitInput").value;
