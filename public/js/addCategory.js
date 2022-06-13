95e0b015746702824ef322f321bdaef6daecb718
Jordan Litsas <jordanlitsas@live.com.au>
2022-06-13 17:35:04 +1000

    button color and input validation

--- public/js/addCategory.js
+++ public/js/addCategory.js
@@ -10,28 +10,32 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const addCategory = async () => {
     let name = document.querySelector("#categoryNameInput").value;
     let sevenDayLimit = document.querySelector("#sevenDayLimitInput").value;
-    let body = {
-        userId: sessionStorage.getItem("userId"),
-        name: name,
-        sevenDayLimit: sevenDayLimit
-    };
+
+    if (name.length != 0 && sevenDayLimit.length != 0){
+        let body = {
+            userId: sessionStorage.getItem("userId"),
+            name: name,
+            sevenDayLimit: sevenDayLimit
+        };
+        
     
-
-    $.ajax({
-        async: true,
-        url:'https://expense-tracker-springboot-api.herokuapp.com/category',
-        contentType: 'application/json',
-        data: JSON.stringify(body),
-        type: 'POST',
-        success: function(data){
-            console.log(data)
-            window.location.replace('./spend.html');
-        }, 
-        error: function(error){
-            console.log('user not inserted')
-            console.log(error)
-
-        }
-    });
+        $.ajax({
+            async: true,
+            url:'https://expense-tracker-springboot-api.herokuapp.com/category',
+            contentType: 'application/json',
+            data: JSON.stringify(body),
+            type: 'POST',
+            success: function(data){
+                console.log(data)
+                window.location.replace('./spend.html');
+            }, 
+            error: function(error){
+                console.log('user not inserted')
+                console.log(error)
+    
+            }
+        });
+    }
+   
 }
 
--- public/js/spend.js
+++ public/js/spend.js
@@ -14,35 +14,40 @@ const addCategory = async () => {
 const submitExpense = async () => {
     let category = document.querySelector("#categoryInput");
     let amountSpent = document.querySelector('#amountSpentInput');
-    let date = new Date();
-    let body = {
-        userId: sessionStorage.getItem('userId'),
-        category: category.value,
-        amountSpent: parseFloat(amountSpent.value),
-        submitDate: date
-    };
-
+   
+    if (category.value.length != 0 && amountSpent.value.length != 0){
+        
+        let date = new Date();
+        let body = {
+            userId: sessionStorage.getItem('userId'),
+            category: category.value,
+            amountSpent: parseFloat(amountSpent.value),
+            submitDate: date
+        };
     
-    $.ajax({
-        async: true,
-        url:'https://expense-tracker-springboot-api.herokuapp.com/spend',
-        contentType: 'application/json',
-        data: JSON.stringify(body),
-        type: 'POST',
-        success: function(data){
-            console.log(data)
-            M.toast({html: "Your expense was recorded."})
-            console.log(category);
-            category.selectedIndex = 0;
-            setupSelect();
-            amountSpent.value = "";
-        }, 
-        error: function(error){
-            console.log('user not inserted')
-            console.log(error)
-
-        }
-    });
+        
+        $.ajax({
+            async: true,
+            url:'https://expense-tracker-springboot-api.herokuapp.com/spend',
+            contentType: 'application/json',
+            data: JSON.stringify(body),
+            type: 'POST',
+            success: function(data){
+                console.log(data)
+                M.toast({html: "Your expense was recorded."})
+                console.log(category);
+                category.selectedIndex = 0;
+                setupSelect();
+                amountSpent.value = "";
+            }, 
+            error: function(error){
+                console.log('user not inserted')
+                console.log(error)
+    
+            }
+        });
+    }
+    
 }
 
 const setupSelect = () => {
